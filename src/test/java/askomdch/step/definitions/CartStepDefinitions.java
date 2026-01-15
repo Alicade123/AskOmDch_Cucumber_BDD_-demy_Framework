05501aeda4e6c9d5108ae0be8931be04297b0aa2
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-15 18:33:53 +0200

    Add cart step definitions

---
+++ src/test/java/askomdch/step/definitions/CartStepDefinitions.java
@@ -0,0 +1,24 @@
+package askomdch.step.definitions;
+
+
+import askomdch.context.dependency_injection.TestContext;
+import askomdch.domain.object.Product;
+import askomdch.pages.CartPage;
+import askomdch.pages.PageFactoryManager;
+import io.cucumber.java.en.Then;
+import org.testng.Assert;
+
+public class CartStepDefinitions {
+    private final CartPage cartPage;
+
+    public CartStepDefinitions(TestContext testContext){
+        cartPage = PageFactoryManager.getCartPage(testContext.driver);
+    }
+
+    @Then("I should see {int} {product} in the cart")
+    public void iShouldSeeInTheCart(int quantity, Product product) {
+        Assert.assertEquals(product.getProductName(), cartPage.getProductName());
+        Assert.assertEquals(quantity, cartPage.getProductQuantity());
+    }
+
+}
