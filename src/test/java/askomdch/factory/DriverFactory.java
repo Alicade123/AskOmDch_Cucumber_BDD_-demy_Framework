19c1ee1fde10922e0b23dafc389930aefb2f0f6a
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-15 18:33:20 +0200

    Update DriverFactory

--- src/test/java/askomdch/factory/DriverFactory.java
+++ src/test/java/askomdch/factory/DriverFactory.java
@@ -1,16 +1,27 @@
 package askomdch.factory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
-    private static WebDriver driver;
-    public static WebDriver initializeDriver(){
-        driver = new ChromeDriver();
+    public static WebDriver initializeDriver(String browser){
+        WebDriver driver;
+        switch (browser) {
+            case "chrome" -> {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+            default -> throw new IllegalStateException("INVALID BROWSER: " + browser);
+        }
         driver.manage().window().maximize();
         return driver;
     }
-    public static WebDriver getDriver(){
-        return driver;
-    }
+
+
 }
