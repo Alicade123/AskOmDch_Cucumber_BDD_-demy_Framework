c35c8b6766a797a1abe404c715ae02f649080fe0
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-15 18:33:20 +0200

    Add parallel driver factory

---
+++ src/test/java/askomdch/factory/DriverFactoryForPARALLEL.java
@@ -0,0 +1,31 @@
+package askomdch.factory;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class DriverFactoryForPARALLEL {
+    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    public static WebDriver initializeDriver(String browser){
+        WebDriver driver;
+        switch (browser) {
+            case "chrome" -> {
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+            }
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+            }
+            default -> throw new IllegalStateException("INVALID BROWSER: " + browser);
+        }
+        driver.manage().window().maximize();
+        DriverFactoryForPARALLEL.driver.set(driver);
+        return driver;
+    }
+
+    public static WebDriver getDriver(){
+        return driver.get();
+    }
+}
