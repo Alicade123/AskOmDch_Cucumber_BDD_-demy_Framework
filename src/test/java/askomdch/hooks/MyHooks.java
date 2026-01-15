1df6e3de4e146f4a73fdf8deab2d14a097e9b96c
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-01-15 18:34:04 +0200

    Update cucumber hooks

--- src/test/java/askomdch/hooks/MyHooks.java
+++ src/test/java/askomdch/hooks/MyHooks.java
@@ -1,18 +1,34 @@
 package askomdch.hooks;
 
+
+import askomdch.context.dependency_injection.TestContext;
 import askomdch.factory.DriverFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import org.openqa.selenium.WebDriver;
+
 
 public class MyHooks {
     private WebDriver driver;
+    private final TestContext testContext;
+
+    public MyHooks(TestContext testContext){
+        this.testContext = testContext;
+    }
+
     @Before
-    public void before(){
-        driver = DriverFactory.initializeDriver();
+    public void before(Scenario scenario){
+        testContext.scenarioName = scenario.getName();
+        System.out.println("BEFORE: THREAD ID : " + Thread.currentThread().threadId() + "," +
+                "SCENARIO NAME: " + scenario.getName());
+        driver = DriverFactory.initializeDriver(System.getProperty("browser", "chrome"));
+        testContext.driver = driver;
     }
     @After
-    public void after(){
+    public void after(Scenario scenario){
+        System.out.println("AFTER: THREAD ID : " + Thread.currentThread().threadId() + "," +
+                "SCENARIO NAME: " + scenario.getName());
         driver.quit();
     }
 }
